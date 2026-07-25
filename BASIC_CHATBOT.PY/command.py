974c5214555397c3bcd4c9c99429caa90ad582c8
Nitin Nagarwal <nagarwalnitin898@gmail.com>
2026-07-25 23:20:49 +0530

    Delete BASIC_CHATBOT.PY directory

--- BASIC_CHATBOT.PY/command.py
+++
@@ -1,9 +0,0 @@
-commands = {
-    "hello": "Greeting",
-    "time": "Show current time",
-    "date": "Show today's date",
-    "calculator": "Perform calculation",
-    "joke": "Tell a joke",
-    "fact": "Show a fact",
-    "bye": "Exit assistant"
-}
